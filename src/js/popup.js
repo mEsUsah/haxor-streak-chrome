003054acedfad42668b6e95d4761c97fe2175200
mEsUsah <stanley@haxor.no>
2024-04-27 22:00:36 +0200

    chore: introduce vue app

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,6 @@
+import { createApp } from 'vue';
+import App from "./App.vue";
+
 (() => {
     const platforms = [
         {
@@ -55,3 +58,7 @@
         });
     });
 })();
+
+const app = createApp(App);
+app.mount("#app");
+
