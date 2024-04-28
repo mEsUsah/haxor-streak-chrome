5e885582ca6919e111928040a36a18d3e3e8bc67
mEsUsah <stanley@haxor.no>
2024-04-28 17:05:07 +0200

    fix(task-list): disable buttons on tasks with registration today

--- src/js/views/TaskList.vue
+++ src/js/views/TaskList.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="task-list__wrapper">
         <div v-for="task in tasks" :key="task.id">
-            <button>&plus;</button> {{ task.name }} ({{ task.streak }}) 
+            <button :disabled="hasRegisteredToday(task)">&plus;</button> {{ task.name }} ({{ task.streak }})           
         </div>
     </div>
-  
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
+
+interface Task {
+    id: number;
+    name: string;
+    streak: number;
+    newest_registration: string;
+}
 
 export default defineComponent({
     props: {
@@ -17,5 +23,10 @@ export default defineComponent({
             required: true
         }
     },
+    methods: {
+        hasRegisteredToday(task: Task): boolean {
+            return task.newest_registration == new Date().toISOString().split('T')[0];
+        }
+    }
 });
 </script>
